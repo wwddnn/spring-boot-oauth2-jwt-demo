d537b803c7337dc7c59399c0d182fcf6eeaf2570
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2024-12-07 22:45:57 -0300

    Spring Security

---
+++ src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.devsuperior.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    //SecurityFilterChain não é o tipo de um método da nossa entidade, por isso usamos o @Bean aqui.
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+}
