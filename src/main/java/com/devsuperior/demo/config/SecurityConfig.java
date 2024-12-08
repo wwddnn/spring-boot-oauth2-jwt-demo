a97fc789c79db089d6aedf58555f3856e16073ad
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2024-12-07 23:08:19 -0300

    BCrypt password

--- src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -3,10 +3,20 @@ package com.devsuperior.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+//A classe de configuração é uma classe onde posso definir componentes(@Bean) na forma de métodos.
 @Configuration
 public class SecurityConfig {
+
+    //É um componente que pode ser injetado em vários lugares. Serve para criar um hash na senha.
+    //PasswordEncoder é a interface e posso trocá-la. O new BCryptPasswordEncoder é a implementação.
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     //SecurityFilterChain não é o tipo de um método da nossa entidade, por isso usamos o @Bean aqui.
     @Bean
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$hLlWiRcE5sdP6RYcxlFeOObhF96/cVTmPuJLmqefFd8lzAq9akyRi');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$hLlWiRcE5sdP6RYcxlFeOObhF96/cVTmPuJLmqefFd8lzAq9akyRi');
 
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
